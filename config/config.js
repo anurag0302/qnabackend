3cc043d7aa163d5b2f3638124499ce35cc5d332c
anuragchhetri <anurag.chhetri@nagarro.com>
2023-03-30 14:30:16 +0530

    key change

--- config/config.js
+++ config/config.js
@@ -1,6 +1,11 @@
 module.exports = {
-  aws_table_name: "users",
+  // aws_table_name: "users",
   aws_local_config: {
     //Provide details for local configuration
+  },
+  aws_remote_config: {
+    accessKeyId: process.env.ACCESS_KEY_ID,
+    secretAccessKey: process.env.ACCESS_SECRET_KEY,
+    region: process.env.AWS_REGION,
   },
 };
